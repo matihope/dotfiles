8f651e309722b34aa18139bc07f9888bb85cc15b
Mateusz Kołpa <mateusz.kolpa@wp.pl>
2023-04-14 16:30:06 +0200

    Use alias in fihs

--- install.fish
+++ install.fish
@@ -11,17 +11,17 @@ echo ".cfg" >> .gitignore
 git clone --bare https://github.com/matihope/dotfiles.git $HOME/.cfg
 
 # For this scope
-set config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias conf_path="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
 # If there were any configs already,
 # make a backup of them :
 # -- Create appropriate folders first
-$config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs dirname | xargs -I{} mkdir -p .dotfiles-backup/{}
+conf_path checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs dirname | xargs -I{} mkdir -p .dotfiles-backup/{}
 # -- Move files
-$config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} .dotfiles-backup/{}
+conf_path checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} .dotfiles-backup/{}
 
-$config checkout
-$config config --local status.showUntrackedFiles no
+conf_path checkout
+conf_path config --local status.showUntrackedFiles no
 
 # cleanup
 rm install.fish
