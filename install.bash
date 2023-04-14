9f5866fa8b78deb470fe949d13be89411a199377
Mateusz Kołpa <mateusz.kolpa@wp.pl>
2023-04-14 16:13:20 +0200

    install.bash improved

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/fish
 
 # Thanks to https://www.atlassian.com/git/tutorials/dotfiles
 # for their great tutorial!
@@ -11,16 +11,17 @@ echo ".cfg" >> .gitignore
 git clone --bare https://github.com/matihope/dotfiles.git $HOME/.cfg
 
 # For this scope
-config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+set config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 # If there were any configs already,
-# make a backup of them.
-mkdir -p .config-backup && \
-$config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .config-backup/{}
+# make a backup of them :
+# -- Create appropriate folders first
+config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs dirname | xargs -I{} mkdir -p .dotfiles-backup/{}
+# -- Move files
+config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} .dotfiles-backup/{}
 
-$config checkout
-$config config --local status.showUntrackedFiles no
+config checkout
+config config --local status.showUntrackedFiles no
 
 # cleanup
 rm install.bash
